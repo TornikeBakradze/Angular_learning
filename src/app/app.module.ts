480a95eca32d2c0cf8092dfbb63b0b9da5de3a4c
tornikeBakradze <tornike.bakradze758@ens.tsu.edu.ge>
2023-11-02 02:07:25 +0400

    restore old Branch and fixed all problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PersonListComponent} from "./person-list/person-list.component";
+import {EditPersonComponent} from "./person-list/edit-person/edit-person.component";
+import {PersonAddressComponent} from "./person-list/edit-person/person-address/person-address.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonListComponent,
+    EditPersonComponent,
+    PersonAddressComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
