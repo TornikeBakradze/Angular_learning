6933bb05a8213be72fc364ec0c733feea9f5baca
t.bakradze <t.bakradze@oppa.ge>
2023-11-01 17:31:11 +0400

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Observer1Component } from './rxjs/observer1/observer1.component';
+import { Observer2Component } from './rxjs/observer2/observer2.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Observer1Component,
+    Observer2Component
   ],
   imports: [
     BrowserModule,
