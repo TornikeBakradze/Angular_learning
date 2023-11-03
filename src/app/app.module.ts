e67f38828367d81add32fe32cf3eb30ad962d2bb
t.bakradze <t.bakradze@oppa.ge>
2023-11-03 16:14:04 +0400

    create Forms without angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
-import { Observer1Component } from './rxjs/observer1/observer1.component';
-import { Observer2Component } from './rxjs/observer2/observer2.component';
+
 import {HttpClientModule} from "@angular/common/http";
+import {MainComponent} from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Observer1Component,
-    Observer2Component
+    MainComponent
   ],
   imports: [
     BrowserModule,
@@ -22,4 +21,5 @@ import {HttpClientModule} from "@angular/common/http";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
