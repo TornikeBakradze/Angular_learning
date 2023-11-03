c6cc94c6853e860ef76ff2da356524ad18ca4da8
t.bakradze <t.bakradze@oppa.ge>
2023-11-03 16:15:18 +0400

    create Forms without angular

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+
+export class MainComponent {
+
+}
